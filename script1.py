5f5efb010f8657ad368c6702e4810bc937c77d89
Adwaitsharadmore <amore14@asu.edu>
2024-05-21 23:21:50 +0530

    Initial commit

---
+++ script1.py
@@ -0,0 +1,8 @@
+
+# Random Python Script
+
+def tyknngcbpr():
+    print("This is a random function called tyknngcbpr.")
+
+if __name__ == "__main__":
+    tyknngcbpr()
---
+++ script2.py
@@ -0,0 +1,8 @@
+
+# Random Python Script
+
+def nvudpxnzlp():
+    print("This is a random function called nvudpxnzlp.")
+
+if __name__ == "__main__":
+    nvudpxnzlp()
